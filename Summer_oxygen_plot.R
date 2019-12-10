e9bed107a39a714f77787473227573f349f32922
Kenneth Thorø Martinsen <kenneth2810@gmail.com>
2019-12-10 12:45:53 +0100

    Added plot of longterm oxygen and temperature. Added plot of attribute vars for summer 2018.

--- Summer_oxygen_plot.R
+++ Summer_oxygen_plot.R
@@ -1,4 +1,4 @@
-library(tidyverse);library(lubridate);library(zoo)
+library(tidyverse);library(lubridate);library(zoo);library(patchwork)
 Sys.setenv(TZ="GMT")
 
 #Aq. Sci.:For most journals the figures should be 39 mm, 84 mm, 129 mm, or 174 mm wide and not higher than 234 mm.
@@ -45,36 +45,71 @@ raw_df <- bind_rows(raw_list, .id = "file") %>%
 #save intermediary .rds file
 #saveRDS(raw_df, paste0(minidot_path, "ilt_raw_df.rds"))
 
-month_pos <- c(5, 6, 7, 8, 9)*c(31, 30, 31, 31, 30)*24*60
-month_labels <- c("May", "June", "July", "August", "September")
+year_labels <- data.frame(year = seq(2013, 2018, 1), labels = c("2013", "2014", "2015", "2016*", "2017", "2018"), x = -Inf, y = Inf)
 
-year_labels <- data.frame(year = seq(2013, 2018, 1), labels = seq(2013, 2018, 1), x = -Inf, y = Inf)
-
-raw_df %>% 
-  mutate(month = month(DateTime_UTC),
+plot_data <- raw_df %>% 
+  mutate(DateTime_UTC = round_date(DateTime_UTC, "10 mins"), #hvad sker der her ved genstart?!?!
+         month = month(DateTime_UTC),
          year = year(DateTime_UTC),
          min_of_year = as.numeric(DateTime_UTC - floor_date(DateTime_UTC, "year")),
-         hob_label = factor(ifelse(hob > 100, "Top", "Bottom"), levels = c("Top", "Bottom"))) %>% 
-  filter(month %in% c(6, 7, 8, 9)) %>% 
+         hob_label = factor(ifelse(hob >= 100, "Top", "Bottom"), levels = c("Top", "Bottom")),
+         day_of_month = mday(DateTime_UTC)) %>% 
+  filter(month %in% c(6, 7, 8)) #%>% 
+  #mutate_at(vars(wtr_doobs, doobs, dosat_perc), funs(rollmean(., 12, align="left", fill = NA)))
+
+plot_data_clean <- plot_data %>% 
+  filter(DateTime_UTC > ymd_hm("2013-06-20 00:00"),
+         !between(DateTime_UTC, ymd_hm("2018-06-01 00:00"),ymd_hm("2018-06-08 00:00"))) %>% 
+  group_by(year, hob) %>% 
+  mutate(do_diff = c(NA, diff(dosat_perc))) %>% 
+  filter(between(do_diff, -2, 2)) %>% 
+  mutate(dosat_perc = ifelse(between(DateTime_UTC, ymd_hm("2018-08-06 12:00"), ymd_hm("2018-08-07 12:00")), NA, dosat_perc),
+         wtr_doobs = ifelse(between(DateTime_UTC, ymd_hm("2018-08-06 12:00"), ymd_hm("2018-08-07 12:00")), NA, wtr_doobs)) %>% 
+  ungroup()
+
+month_pos <- plot_data %>% 
+  filter(day_of_month == 1) %>% 
+  distinct(month, day_of_month, .keep_all = TRUE) %>% 
+  pull(min_of_year) %>% 
+  c(., 20995200)
+
+month_labels <- c("June", "July", "August", "September")
+
+plot_data_avg_dif <- plot_data_clean %>% 
+  group_by(year, min_of_year) %>% 
+  summarise(wtr_doobs_mean = mean(wtr_doobs, na.rm = TRUE)) %>% 
+  mutate(wtr_avg_dif = wtr_doobs_mean - mean(wtr_doobs_mean, na.rm = TRUE))
+
+plot_data_clean %>% 
   ggplot(aes(min_of_year, dosat_perc, col = hob_label))+
-  #ggplot(aes(min_of_year, dosat_perc, group = hob_label, col = dosat_perc))+
   geom_line()+
   scale_x_continuous(breaks = month_pos, labels = month_labels)+
   ylab("Dissolved oxygen saturation (%)")+
   xlab(NULL)+
-  #scale_color_gradient2(low = "#A50026", high = "#006837", mid = "cornsilk2", midpoint = 100, limits = c(-20, 200))+
-  #sc+
-  scale_color_manual(values = c("grey", "black"))+
+  scale_color_manual(values = c("grey", "black"), name = "Sensor position")+
   geom_text(data = year_labels, aes(x = x, y = y, label = labels), inherit.aes = FALSE, hjust = -0.2, vjust = 1.2)+
   facet_grid(year~.)+
   theme(strip.text = element_blank(),
-        legend.title = element_blank(),
-        legend.position = "bottom")
+        legend.position = "bottom")+
+  guides(color = guide_legend(title.position="top"))
 
-ggsave(paste0(minidot_path, "ilt_plot.png"), height = 234, width = 174, units = "mm")
+ggsave(paste0(getwd(), "/ilt_plot.png"), height = 234, width = 174, units = "mm")
 
-library(RColorBrewer)
-myPalette <- colorRampPalette(c("#A50026", "#F46D43", "#FEE08B", "#92C5DE", "#D9EF8B", "#66BD63", "#006837"))
-sc <- scale_colour_gradientn(colours = myPalette(100))
-display.brewer.pal(10, "Spectral")
-brewer.pal(10, "RdYlGn")
+plot_data_avg_dif %>% 
+  ggplot(aes(min_of_year, wtr_doobs_mean, col = wtr_avg_dif))+
+  geom_line()+
+  scale_x_continuous(breaks = month_pos, labels = month_labels)+
+  ylab(expression("Mean watercolumn temperature ("*degree*C*")"))+
+  xlab(NULL)+
+  #scale_colour_distiller(type = "div", palette = "RdYlBu")+
+  scale_colour_gradient2(low = "cornflowerblue", high = "brown1", mid = "grey", breaks = c(-6, -3, 0, 3, 6), limits = c(-8, 8))+
+  geom_text(data = year_labels, aes(x = x, y = y, label = labels), inherit.aes = FALSE, hjust = -0.2, vjust = 1.2)+
+  facet_grid(year~.)+
+  theme(strip.text = element_blank(),
+        legend.position = "bottom",
+        legend.box = "horizontal")+
+  guides(color = guide_colourbar(title.position="top", title = expression(Temperature~anomaly~"("*degree*C*")"), barwidth = unit(45, "mm")))
+
+ggsave(paste0(getwd(), "/wtr_plot.png"), height = 234, width = 174, units = "mm")
+
+
---
+++ attr_vars_summer_2018.R
@@ -0,0 +1,78 @@
+library(tidyverse);library(lubridate);library(zoo);library(patchwork);library(readxl)
+Sys.setenv(TZ="GMT")
+Sys.setlocale("LC_TIME", "US")
+
+#ggplot theme
+theme_pub <- theme_bw() + 
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.text = element_text(colour = "black"), 
+        strip.background = element_rect(fill = "white"))
+theme_set(theme_pub)
+
+vejrst_2018_df <- read.csv(paste0(getwd(), "/Rawdata/Vejrstation_Filsoe_18-09-12.csv")) %>% 
+  tbl_df() %>% 
+  set_names(c("DateTime_GMT2", "atmpres", "wnd", "wnd_gust", "wnd_dir", "par", "rain", "airt", "rh")) %>% 
+  mutate(DateTime_UTC = dmy_hms(DateTime_GMT2)-2*60*60,
+         par = par-1.2)
+
+df_wnd_rain_airt <- vejrst_2018_df %>% 
+  select(DateTime_UTC, rain, wnd, airt) %>% 
+  filter(month(DateTime_UTC) %in% c(6, 7, 8)) %>% 
+  #mutate(DateTime_UTC = round_date(DateTime_UTC, "24 hour")) %>% 
+  mutate(date = as_date(DateTime_UTC)) %>% 
+  na.omit() %>% 
+  select(-DateTime_UTC) %>% 
+  group_by(date) %>% 
+  summarise_all(funs(min, mean, max, sum))
+
+df_doc <- read_xlsx(paste0(getwd(), "/Rawdata/filso_doc.xlsx"), sheet = 2) %>% 
+  mutate(date = as_date(Date)) %>%
+  na.omit() %>% 
+  group_by(date) %>% 
+  summarise(doc = mean(konc_mg_l1))
+
+airt <- df_wnd_rain_airt %>% 
+  ggplot(aes(x=date, y=airt_mean, ymin=airt_min, ymax=airt_max))+
+  geom_ribbon(fill=grey(0.7))+
+  geom_line()+
+  ylab(expression("Air temperature ("*degree*C*")"))+
+  xlab(NULL) +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())+
+  scale_x_date(limits = c(ymd("2018-06-01"), ymd("2018-09-01")))
+
+wnd <- df_wnd_rain_airt %>% 
+  ggplot(aes(x=date, y=wnd_mean, ymin=wnd_min, ymax=wnd_max))+
+  geom_ribbon(fill=grey(0.7))+
+  geom_line()+
+  ylab(expression("Wind speed ("*m~s^{-1}*")"))+
+  xlab(NULL) +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())+
+  scale_x_date(limits = c(ymd("2018-06-01"), ymd("2018-09-01")))+
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 10), breaks = seq(0, 8, 2))
+
+rain <- df_wnd_rain_airt %>% 
+  ggplot(aes(date, rain_sum))+
+  geom_col(fill = "black", col = "white")+
+  ylab("Precipitation (mm)")+
+  xlab(NULL) +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())+
+  scale_x_date(limits = c(ymd("2018-06-01"), ymd("2018-09-01")))+
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 40))
+
+doc <- df_doc %>% 
+  filter(month(date) %in% c(6, 7, 8)) %>% 
+  ggplot(aes(date, doc))+
+  geom_line()+
+  geom_point()+
+  ylab(expression("DOC (mg"~L^{-1}*")"))+
+  xlab(NULL)+
+  scale_x_date(date_labels = "%B", limits = c(ymd("2018-06-01"), ymd("2018-09-01")))
+
+all_plots <- airt+wnd+rain+doc+plot_layout(ncol=1)
+all_plots
+
+ggsave(paste0(getwd(), "/attr_vars_plot.png"), all_plots, height = 170, width = 129, units = "mm")
